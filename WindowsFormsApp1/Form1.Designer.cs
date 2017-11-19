5ede8e89d34fa4198d4bd9a7f970ede23f80e894
fillu87gyc <s1500604@sendai.kosen-ac.jp>
2017-11-20 08:28:50 +0900

    寮食ボット用に画像を切り分けます

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -28,12 +28,54 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.components = new System.ComponentModel.Container();
+			this.button1 = new System.Windows.Forms.Button();
+			this.button2 = new System.Windows.Forms.Button();
+			this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+			this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+			this.SuspendLayout();
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(12, 12);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(170, 50);
+			this.button1.TabIndex = 1;
+			this.button1.Text = "ファイルを選択する";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(12, 68);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(170, 50);
+			this.button2.TabIndex = 2;
+			this.button2.Text = "ファイルを開く";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.button2_Click);
+			// 
+			// openFileDialog1
+			// 
+			this.openFileDialog1.FileName = "openFileDialog1";
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(192, 134);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.button1);
+			this.Name = "Form1";
 			this.Text = "Form1";
+			this.ResumeLayout(false);
+
 		}
 
 		#endregion
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+		private System.Windows.Forms.OpenFileDialog openFileDialog1;
 	}
 }
 
--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -7,14 +7,45 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
 namespace WindowsFormsApp1
 {
 	public partial class Form1 : Form
 	{
-		public Form1()
+		public Form1() => InitializeComponent();
+
+		private void button1_Click(object sender, EventArgs e)
 		{
-			InitializeComponent();
+			//OpenFileDialogクラスのインスタンスを作成
+			OpenFileDialog ofd = new OpenFileDialog
+			{
+				InitialDirectory = @"D:\Drive\Google ドライブ\寮食メニュー\生データ",
+			
+				Filter = "すべてのファイル(*.*)|*.*",
+				FilterIndex = 2,
+				Title = "開くファイルを選択してください",
+				RestoreDirectory = true,
+				CheckFileExists = true,
+				CheckPathExists = true
+			};
+			ofd.ShowDialog();
+			Bitmap img = new Bitmap(ofd.FileName);
+			int width = img.Width / 7;
+			int height = img.Height / 3;
+			for (int row = 0; row < 7; row++)
+			{
+				for (int column = 0; column < 3; column++)
+				{					
+					Rectangle Rect = new Rectangle((row * width), (column * height), width, height);
+					var newimg = img.Clone(Rect, img.PixelFormat);
+					newimg.Save(local + row + column + ".gif", System.Drawing.Imaging.ImageFormat.Gif);
+					newimg.Dispose();
+				}
+			}
+		}
+		string local = @"D:\Drive\Google ドライブ\寮食メニュー\加工済み\";
+		private void button2_Click(object sender, EventArgs e)
+		{
+			System.Diagnostics.Process.Start(local);
 		}
 	}
 }
