3da84d9abb9ac7e32fc4af7f2eb0ca31fcf7dd40
fillu87gyc <s1500604@sendai.kosen-ac.jp>
2017-12-17 22:13:46 +0900

    縦長な場合、回転する

--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -19,7 +19,7 @@ namespace WindowsFormsApp1
 			OpenFileDialog ofd = new OpenFileDialog
 			{
 				InitialDirectory = @"D:\Drive\Google ドライブ\寮食メニュー\生データ",
-			
+
 				Filter = "すべてのファイル(*.*)|*.*",
 				FilterIndex = 2,
 				Title = "開くファイルを選択してください",
@@ -29,12 +29,26 @@ namespace WindowsFormsApp1
 			};
 			if (ofd.ShowDialog() == DialogResult.Cancel) return;
 			Bitmap img = new Bitmap(ofd.FileName);
+
+			if (img.Width < img.Height)
+			{
+				string message = "時計回りに回転ならYES、反時計ならNo";
+				string caption = "ファイルを回転させてください";
+				MessageBoxButtons buttons = MessageBoxButtons.YesNo;
+				var result = MessageBox.Show(this, message, caption, buttons);
+
+				if (result == DialogResult.Yes)
+					img.RotateFlip(RotateFlipType.Rotate90FlipNone);
+				else
+					img.RotateFlip(RotateFlipType.Rotate270FlipNone);
+			}
+
 			int width = img.Width / 7;
 			int height = img.Height / 3;
 			for (int row = 0; row < 7; row++)
 			{
 				for (int column = 0; column < 3; column++)
-				{					
+				{
 					Rectangle Rect = new Rectangle((row * width), (column * height), width, height);
 					var newimg = img.Clone(Rect, img.PixelFormat);
 					newimg.Save(local + row + column + ".gif", System.Drawing.Imaging.ImageFormat.Gif);
